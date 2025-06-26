3ac56f5d64973bb9fefd7cbdd89c6305eeb87df5
battiatomatteo <battiatomatteo29@gmail.com>
2025-06-26 17:41:16 +0200

    img per icona app

--- src/main/java/view/LogInView.java
+++ src/main/java/view/LogInView.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class LogInView extends Application {
@@ -11,6 +12,7 @@ public class LogInView extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/LogIn.fxml"));
         primaryStage.setTitle("Login");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/img/icona_dottore.jpg")));
         primaryStage.setScene(new Scene(root, 350, 200));
         primaryStage.show();
     }
