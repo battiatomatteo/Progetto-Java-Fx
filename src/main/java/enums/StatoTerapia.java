9048628d9c49d11710bd4bc420763e9096941a7e
Beghinins <107865447+Beghinins@users.noreply.github.com>
2025-08-07 18:55:03 +0200

    Add files via upload

--- src/main/java/enums/StatoTerapia.java
+++ src/main/java/enums/StatoTerapia.java
@@ -8,6 +8,15 @@ public enum StatoTerapia {
     public String getStato() {
         return this.name();
     }
+
+   public static String[] getAll() {
+       String[] values = new String[StatoTerapia.values().length];
+       for (StatoTerapia s : StatoTerapia.values()) {
+           values[s.ordinal()] = s.name();
+       }
+       return values;
+   }
+
 }
 
 
